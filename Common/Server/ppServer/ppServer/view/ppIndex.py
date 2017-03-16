c83a8709d53abac7ede36f4d392d41a9b9455719
Cyndi <Cyndi@10.com>
2017-03-16 14:16:52 +0800

    222
    332232

--- Common/Server/ppServer/ppServer/view/ppIndex.py
+++ Common/Server/ppServer/ppServer/view/ppIndex.py
@@ -5,9 +5,35 @@ from django.shortcuts import render
 
 from ppServer.view.ppAnalyze import read_file
 
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def dir_list(path, allfile, result_object):
+    filelist = os.listdir(path)
+
+
+    for filename in filelist:
+        filepath = os.path.join(path, filename)
+        if os.path.isdir(filepath):
+            dir_list(filepath, allfile, result_object)
+        else:
+            allfile.append(filepath)
+            if '.py' in filepath and '.pyc' not in filepath:
+
+                result_object.append(read_file(filepath))
+
+
+
+    return result_object
+
+
 def get_inspector(request):
-    # return HttpResponse('hello world!')
     context = {}
-    context['inspectorObject'] = read_file()
+
+    result_object = []
+    dir_list(BASE_DIR.replace('/Common/Server/ppServer/ppServer', ''), [], result_object)
+    context['inspectorObject'] = result_object
+
+    # context['inspectorObject'] = read_file()
 
     return render(request, 'inspector.html', context)
